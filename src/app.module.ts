3ee0a39e0d69ef0370eb9d6274e3f0d0506d26c6
Juma Antony <jumaantony840@gmail.com>
2023-08-01 11:55:02 +0300

    configure mongodb and added lesson entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { LessonResolvers } from './lesson/lesson.resolvers';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
 
 @Module({
   imports: [
+
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
+
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: true,
       driver: ApolloDriver,
